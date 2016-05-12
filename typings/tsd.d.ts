713df3e2d81757a6c1d7efd46605077840d9773f
James Rees <james.rees@outlook.com>
2016-05-12 23:43:51 +0100

    Fixed some typings issues and added a .gitignore, Will need to manuall remove some d.ts files.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,10 +1,7 @@
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="express/express.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
 /// <reference path="mongoose/mongoose.d.ts" />
-/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="express-serve-static-core/express-serve-static-core.d.ts" />
+/// <reference path="express/express.d.ts" />
 /// <reference path="mime/mime.d.ts" />
-/// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="promises-a-plus/promises-a-plus.d.ts" />
-
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
